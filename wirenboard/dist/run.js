dd8c0c64b3e04db445d180e1ee2e64830cc61fe7
Dmitriy Borodin <dmitriy@borodin.site>
2022-08-29 20:54:03 +0300

    fix

--- wirenboard/dist/run.js
+++ wirenboard/dist/run.js
@@ -4,13 +4,13 @@ const node_child_process_1 = require("node:child_process");
 const run = async () => {
     const ls = (0, node_child_process_1.spawn)("/root/node/bin/node", ["/root/butler/wirenboard/dist/index.js"]);
     ls.stdout.on("data", (data) => {
-        console.log(data);
+        console.log(data.toString());
     });
     ls.stderr.on("data", (data) => {
-        console.error(data);
+        console.error(data.toString());
     });
     ls.on("close", (code) => {
-        console.log(`Child process exited with code ${code}`);
+        console.log(`Cyhild process exited with code ${code}`);
     });
 };
 run();
--- wirenboard/src/run.ts
+++ wirenboard/src/run.ts
@@ -4,11 +4,11 @@ const run = async () => {
   const ls = spawn("/root/node/bin/node", ["/root/butler/wirenboard/dist/index.js"]);
 
   ls.stdout.on("data", (data) => {
-    console.log(data);
+    console.log(data.toString());
   });
 
   ls.stderr.on("data", (data) => {
-    console.error(data);
+    console.error(data.toString());
   });
 
   ls.on("close", (code) => {
