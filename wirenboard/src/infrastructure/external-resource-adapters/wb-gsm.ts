8f03a49b72bf4237795090751a0609b8aed37f8f
Dmitriy Borodin <dmitriy@borodin.site>
2022-08-29 13:04:22 +0300

    fix

--- wirenboard/src/infrastructure/external-resource-adapters/wb-gsm.ts
+++ wirenboard/src/infrastructure/external-resource-adapters/wb-gsm.ts
@@ -35,6 +35,18 @@ export const wbGsm = async ({ logger, signal }: WbGsmParams) => {
         console.log(stdout);
       });
 
+      childProcess.on("error", (error) => {
+        logger.error({ err: error });
+        console.error(error);
+      });
+
+      childProcess.on("close", (code) => {
+        const message = `The wb-gsm restart_if_broken process was closed with code: ${code}`;
+
+        logger.info({ code }, message);
+        console.log(message);
+      });
+
       const timer = setTimeout(() => {
         const message =
           "The wb-gsm restart_if_broken process does not finish for more than 2 minutes, the process will be forcibly stopped and restarted 🚨";
@@ -42,7 +54,7 @@ export const wbGsm = async ({ logger, signal }: WbGsmParams) => {
         logger.info(message);
         console.log(message);
 
-        childProcess.kill("SIGTERM");
+        childProcess.kill("SIGINT");
       }, 2 * 60 * 1000);
 
       const isExit = await new Promise((resolve) => {
