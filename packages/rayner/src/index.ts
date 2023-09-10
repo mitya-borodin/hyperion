fa20eb79acd0cbd6d42e14837f18bf991113c3e7
Dmitriy Borodin <dmitriy@borodin.site>
2023-09-10 21:43:02 +0300

    fix: add docker compose and cmd + bin dirs

--- packages/rayner/src/index.ts
+++ packages/rayner/src/index.ts
@@ -10,37 +10,39 @@ import { waitSeedingComplete } from './infrastructure/postgres/repository/helper
 import { WirenboardDeviceRepository } from './infrastructure/postgres/repository/wirenboard-device-repository';
 import { createHttpInterface } from './interfaces/http';
 
-entrypoint(async ({ signal, logger, defer }) => {
-  const prismaClient = new PrismaClient();
+export const run = () => {
+  entrypoint(async ({ signal, logger, defer }) => {
+    const prismaClient = new PrismaClient();
 
-  await waitSeedingComplete({ signal, logger, prismaClient });
+    await waitSeedingComplete({ signal, logger, prismaClient });
 
-  const wirenboardDeviceRepository = new WirenboardDeviceRepository({ logger, client: prismaClient });
+    const wirenboardDeviceRepository = new WirenboardDeviceRepository({ logger, client: prismaClient });
 
-  const wirenboard = await runWirenboard({ config, logger: logger.child({ name: 'wirenboard' }) });
+    const wirenboard = await runWirenboard({ config, logger: logger.child({ name: 'wirenboard' }) });
 
-  defer(() => wirenboard.stop());
+    defer(() => wirenboard.stop());
 
-  const stopCollectWirenboardDeviceData = runCollectWirenboardDeviceData({
-    logger,
-    pubSub: wirenboard.pubSub,
-    wirenboardDeviceRepository,
+    const stopCollectWirenboardDeviceData = runCollectWirenboardDeviceData({
+      logger,
+      pubSub: wirenboard.pubSub,
+      wirenboardDeviceRepository,
+    });
+
+    defer(() => stopCollectWirenboardDeviceData());
+
+    const fastify = await createHttpInterface({
+      config,
+      logger: logger.child({ name: 'http-server' }),
+    });
+
+    await fastify.listen({
+      host: config.fastify.host,
+      port: config.fastify.port,
+    });
+
+    defer(() => fastify.close());
+    defer(() => prismaClient.$disconnect());
+
+    await forever(signal);
   });
-
-  defer(() => stopCollectWirenboardDeviceData());
-
-  const fastify = await createHttpInterface({
-    config,
-    logger: logger.child({ name: 'http-server' }),
-  });
-
-  await fastify.listen({
-    host: config.fastify.host,
-    port: config.fastify.port,
-  });
-
-  defer(() => fastify.close());
-  defer(() => prismaClient.$disconnect());
-
-  await forever(signal);
-});
+};
