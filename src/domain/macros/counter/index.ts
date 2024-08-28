f611d4466d29308abe60b4b10795a9e635b8dc56
Dmitriy Borodin <dmitriy@borodin.site>
2024-08-28 20:47:54 +0300

    Update counter definitions

--- src/domain/macros/counter/index.ts
+++ src/domain/macros/counter/index.ts
@@ -7,14 +7,41 @@ import { Macros, MacrosParameters } from '../macros';
 import { MacrosType } from '../showcase';
 
 const logger = getLogger('hyperion:macros:counter');
+
 /**
  * ! SETTINGS
  */
+
 /**
  * Тип счетчика, по типу уже определяется единица измерения и поведение.
  */
 export enum CounterType {
-  UNSPECIFIED = 'UNSPECIFIED',
+  /**
+   * Учет количества импульсов.
+   *
+   * Значение по умолчанию.
+   */
+  IMPULSE_COUNT = 'IMPULSE_COUNT',
+
+  /**
+   * Учет количества переключения реле.
+   */
+  RELAY_SWITCH_COUNT = 'RELAY_SWITCH_COUNT',
+
+  /**
+   * Учет электричества, в кило ватах в час.
+   */
+  ELECTRICITY = 'ELECTRICITY',
+
+  /**
+   * Учет газа, в кубических метрах.
+   */
+  GAS = 'GAS',
+
+  /**
+   * Учет тепла, в кило ватах в час.
+   */
+  HEAT = 'HEAT',
 
   /**
    * Учет холодной воды, в кубических метрах.
@@ -25,40 +52,21 @@ export enum CounterType {
    * Учет горячей воды, в кубических метрах.
    */
   HOT_WATER = 'HOT_WATER',
-
-  /**
-   * Учет газа, в кубических метрах.
-   */
-  GAS = 'GAS',
-
-  /**
-   * Учет электричества, в кило ватах в час.
-   */
-  ELECTRICITY = 'ELECTRICITY',
-
-  /**
-   * Учет тепла, в кило ватах в час.
-   */
-  HEAT = 'HEAT',
-
-  /**
-   * Учет времени когда реле во включенном состоянии, в секундах.
-   */
-  RELAY_ON = 'RELAY_ON',
-
-  /**
-   * Учет времени когда реле в выключенном состоянии, в секундах.
-   */
-  RELAY_OFF = 'RELAY_OFF',
-
-  /**
-   * Учет количества переключений, в разах.
-   */
-  SWITCH = 'SWITCH',
 }
 
 export enum UnitOfMeasurement {
-  UNSPECIFIED = 'UNSPECIFIED',
+  /**
+   * Количество переключений.
+   *
+   * Значение по умолчанию.
+   */
+  SWITCHES = 'switches',
+
+  /**
+   * Время работы, сумма временных отрезков, от включения
+   * реле до выключения.
+   */
+  WORK = 'sec',
 
   /**
    * Объем.
@@ -69,16 +77,6 @@ export enum UnitOfMeasurement {
    * Мощность.
    */
   POWER = 'kW/h',
-
-  /**
-   * Время работы, сумма временных отрезков, от включения реле до выключения.
-   */
-  WORK_TIME = 'sec',
-
-  /**
-   * Количество переключений.
-   */
-  TIMES = 'times',
 }
 
 /**
@@ -96,19 +94,31 @@ export enum Trigger {
   BACK = 'BACK',
 
   /**
-   * Реакция только на изменение состояния контакта.
+   * Реакция на изменение состояния контакта.
+   *
+   * Значение по умолчанию, для всех типов CounterType кроме RELAY_SWITCH_COUNT
+   * для RELAY_SWITCH_COUNT значение по умолчанию будет FRONT.
    */
   BOTH = 'BOTH',
 }
 
 /**
  * Импульсный счетчик
- * воды,
- * газа,
- * электричества,
- * тепла,
- * количества (верхних, нижних) уровней на переключателях,
- * время работы и/или простоя реле.
+ *
+ * Это устройство которое работает как ключ который при прохождение определенного
+ * количества ресурса через прибор учета то замыкается то размыкается.
+ *
+ * Отслеживая частоту включений-выключений можно понять скорость расхода ресурса,
+ * посчитав количество переключений можно понять объем расхода ресурса,
+ * отслеживая включенное состояние можно посчитать моточасы устройств подключенных через реле.
+ *
+ * Макрос импульсного счетчика может учитывать:
+ * - Количество импульсов (считается вне зависимости от выбранного типа ресурса)
+ * - Моточасы или время нахождения переключателя в указанном положении (считается независимо от выбранного типа ресурса)
+ * - Электричество
+ * - Газ
+ * - Тепло
+ * - Воду
  */
 export type CounterMacrosSettings = {
   readonly devices: {
@@ -120,9 +130,13 @@ export type CounterMacrosSettings = {
   readonly properties: {
     readonly type: CounterType;
     readonly trigger: Trigger;
+    readonly initOfMeasurement: UnitOfMeasurement;
 
     /**
      * Стоимость одного импульса.
+     *
+     * По умолчанию 0, ресурс не будет учитываться, будет учитываться
+     * только количество переключений и время проведенное в каждом положении.
      */
     readonly price: number;
   };
@@ -131,20 +145,32 @@ export type CounterMacrosSettings = {
 /**
  * ! STATE
  */
-// eslint-disable-next-line @typescript-eslint/ban-types
+/**
+ * Публичное состояние счетчика, на которое пользователь может влиять.
+ */
 export type CounterMacrosPublicState = {
   /**
    * Значение в единицах измерения, которое пользователь может задать, чтобы синхронизовать значение
-   * физического счетчика и счетчика импульсов.
+   * прибора учета и макроса.
    */
   readonly amount: number;
 };
 
+/**
+ * Внутренне состояние счетчика, на которое пользователь НЕ может влиять.
+ */
 type CounterMacrosPrivateState = {
   /**
    * Количество импульсов.
    */
   counter: number;
+
+  /**
+   * Количество секунд засчитанных как рабочее время устройства подключенного к реле.
+   *
+   * Моточасы учитываются только для CounterType => RELAY_SWITCH_COUNT
+   */
+  workSec: number;
 };
 
 type CounterMacrosState = CounterMacrosPublicState & CounterMacrosPrivateState;
@@ -159,6 +185,11 @@ type CounterMacrosNextOutput = {
   readonly counter: number;
 
   /**
+   * Моточасы, > 0 будет только в случае CounterType => RELAY_SWITCH_COUNT.
+   */
+  readonly workSec: number;
+
+  /**
    * Значение в единицах измерения.
    */
   readonly amount: number;
@@ -171,6 +202,7 @@ type CounterMacrosParameters = MacrosParameters<string, string | undefined>;
 
 const defaultState: CounterMacrosState = {
   counter: 0,
+  workSec: 0,
   amount: 0,
 };
 
@@ -211,8 +243,9 @@ export class CounterMacros extends Macros<MacrosType.COUNTER, CounterMacrosSetti
 
     this.output = {
       counter: -1,
+      workSec: -1,
       amount: -1,
-      unitOfMeasurement: UnitOfMeasurement.UNSPECIFIED,
+      unitOfMeasurement: UnitOfMeasurement.SWITCHES,
     };
   }
 
@@ -256,8 +289,9 @@ export class CounterMacros extends Macros<MacrosType.COUNTER, CounterMacrosSetti
   protected computeOutput = () => {
     const output: CounterMacrosNextOutput = {
       counter: -1,
+      workSec: -1,
       amount: -1,
-      unitOfMeasurement: UnitOfMeasurement.UNSPECIFIED,
+      unitOfMeasurement: UnitOfMeasurement.SWITCHES,
     };
 
     this.output = output;
